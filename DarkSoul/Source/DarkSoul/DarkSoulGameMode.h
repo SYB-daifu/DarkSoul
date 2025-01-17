ca5e8698a024b6051a93f5e25e19b3b90be47a4a
黑猫#长 <524175930@qq.com>
2025-01-18 01:57:32 +0800

    ue project

---
+++ DarkSoul/Source/DarkSoul/DarkSoulGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DarkSoulGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADarkSoulGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADarkSoulGameMode();
+};
+
+
+
