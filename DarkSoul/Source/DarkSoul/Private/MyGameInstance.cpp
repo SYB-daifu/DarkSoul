ce0a116169a8b0f0bcf9a2bc3f6890ccb77dfb09
DESKTOP-4EL3V9O\pc <1041962748@qq.com>
2025-01-21 02:55:16 +0800

    【新增】UMyGameInstance类，继承UGameInstance，实现自己的全局单例

---
+++ DarkSoul/Source/DarkSoul/Private/MyGameInstance.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	AppID = "2025.1.21";
+	AppKey = "1008611";
+	AppName = "DarkSoul";
+	AppVersion = "0.0.1";
+}
+
---
+++ DarkSoul/Source/DarkSoul/Public/MyGameInstance.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+// GameInstance是全局唯一单例，这个在引擎初始化的时候就已经生成，一直存在到引擎关闭
+// 主要用于为游戏保存全局临时数据，想要持久化保存需要用SaveGame
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "MyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKSOUL_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// 构造函数
+	UMyGameInstance();
+	// 基本数据成员
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyGameInstance")
+	FString AppID;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyGameInstance")
+	FString AppKey;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyGameInstance")
+	FString AppName;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MyGameInstance")
+	FString AppVersion;
+};
